cad4ebf42c9a6864f0f7bfababc318c47aa6cb7f
Giovanni Soares <giovanni.soares@oderco.com.br>
2023-01-14 18:02:17 -0300

    example nodemailer and publish package

---
+++ examples/nodemailer/src/main.ts
@@ -0,0 +1,28 @@
+import { render } from "@zen-labs/render-vue";
+import View from "./View.vue";
+import nodemailer from "nodemailer";
+
+const props = {
+  id: 1,
+};
+
+(async () => {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "",
+      pass: "",
+    },
+  });
+
+  const emailHtml = await render(View, props);
+
+  const options = {
+    from: "",
+    to: "",
+    subject: "hello world",
+    html: emailHtml,
+  };
+
+  transporter.sendMail(options);
+})();
