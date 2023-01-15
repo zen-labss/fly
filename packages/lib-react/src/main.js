6f44fd03378763242daad48c59fa9327790da225
Giovanni Soares <giovanni.soares@oderco.com.br>
2023-01-14 23:44:40 -0300

    feat: export files jsx

---
+++ packages/lib-react/src/main.js
@@ -0,0 +1,9 @@
+import Button from "./button.jsx";
+import Hr from "./hr.jsx";
+import Html from "./html.jsx";
+import Image from "./img.jsx";
+import Section from "./section.jsx";
+import Text from "./text.jsx";
+import Container from "./container.jsx";
+
+export { Button, Hr, Html, Image, Section, Text, Container };
